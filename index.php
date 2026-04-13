4236e9c0c46472641a99b80e67620c309d43cbb3
giorgio laera <laeragiorg@gmail.com>
2026-04-13 21:30:37 +0200

    generatore di password spostata in una funzione

--- index.php
+++ index.php
@@ -8,21 +8,20 @@
 <body>
     <h1>ciao</h1>
     <?php 
+    function passGenerator ($lenghtPass){
          $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-         var_dump("alfabeto ".$alphabet[1]);
          $pass = array();
-         var_dump($pass);
         $alphaLength = strlen($alphabet) - 1;
-        var_dump($alphaLength);
+
         
-        for ($i = 0; $i < 6; $i++){
+        for ($i = 0; $i < $lenghtPass; $i++){
             $n= rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }; 
 
-        var_dump( implode($pass));
-
-        
+        return implode($pass);
+    };
+    echo passGenerator();
     ?>
 </body>
 </html>
