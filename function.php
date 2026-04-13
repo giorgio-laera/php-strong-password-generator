320c719ba8f99c3528e4b4de7d136124172ae76c
giorgio laera <laeragiorg@gmail.com>
2026-04-13 22:00:35 +0200

    spostato generatore un un file a parte

---
+++ function.php
@@ -0,0 +1,15 @@
+ <?php 
+    function passGenerator ($lengthPass){
+         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+         $pass = array();
+        $alphaLength = strlen($alphabet) - 1;
+
+        
+        for ($i = 0; $i < $lengthPass; $i++){
+            $n= rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }; 
+
+        return implode($pass);
+    };
+    ?>
--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 
     <?php 
-    function passGenerator ($lengthPass){
-         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-         $pass = array();
-        $alphaLength = strlen($alphabet) - 1;
-
-        
-        for ($i = 0; $i < $lengthPass; $i++){
-            $n= rand(0, $alphaLength);
-            $pass[] = $alphabet[$n];
-        }; 
-
-        return implode($pass);
-    };
+   require 'function.php';
     ?>
 
     <?php 
